08cc7ec16590f8d9fe889ac916b7308e6a355a02
Muhammed <muhammed.almazbekovich@gmail.com>
2022-03-11 17:48:05 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Rectangle rectangle = new Rectangle();
+
+    rectangle.setWidth(6);
+    rectangle.setHeight(5);
+        System.out.println("the area of the rectangle is equal to = " + rectangle.method());
+    }
+
+}
---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Rectangle {
+    private int height;
+    private int width;
+
+    int getHeight(){
+        return height;
+    }
+    void setHeight(int height){
+        this.height = height;
+    }
+
+    int getWidth(){
+        return width;
+    }
+    void setWidth(int width){
+        this.width = width;
+
+    }
+     int   method (){
+        return this.width * this.height;
+
+    }
+
+
+
+}
+
+
+
+
+
